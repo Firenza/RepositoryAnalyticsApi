eda732e4b586e7d4c7257c4f85acfb225d40925b
Firenza <chadoftedahl@gmail.com>
2018-01-28 09:48:15 -0600

    Started GitHub integration

---
+++ RepositoryAnaltyicsApi.Interfaces/IRepositorySourceRepository.cs
@@ -0,0 +1,15 @@
+﻿using RepositoryAnalyticsApi.ServiceModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryAnaltyicsApi.Interfaces
+{
+    public interface IRepositorySourceRepository
+    {
+        List<RepositoryFile> ReadFiles(string repositoryId, string branch);
+        List<Repository> ReadRepositories(string group, int pageCount, int pageSize, int startPage);
+        Repository ReadRepository(string repositoryId);
+        string GetFileContent(string repositoryId, string fullFilePath);
+    }
+}
