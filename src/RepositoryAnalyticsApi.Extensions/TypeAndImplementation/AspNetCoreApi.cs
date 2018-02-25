2dfff232476eb22e26e290355dc410f55cf853a9
Firenza <chadoftedahl@gmail.com>
2018-02-24 18:22:16 -0600

    Added type parsers for asp.net core apis and dot net core console apps

---
+++ src/RepositoryAnalyticsApi.Extensions/TypeAndImplementation/AspNetCoreApi.cs
@@ -0,0 +1,46 @@
+﻿using RepositoryAnalyticsApi.Extensibliity;
+using RepositoryAnalyticsApi.ServiceModel;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryAnalyticsApi.Extensions.TypeAndImplementation
+{
+    [Export(typeof(IDeriveRepositoryTypeAndImplementations))]
+    public class AspNetCoreApi : IDeriveRepositoryTypeAndImplementations
+    {
+        public async Task<RepositoryTypeAndImplementations> DeriveImplementationAsync(IEnumerable<RepositoryDependency> dependencies, Func<Task<List<RepositoryFile>>> readFilesAsync, IEnumerable<string> topics, string name, Func<string, Task<string>> readFileContentAsync)
+        {
+            var nugetDependencies = dependencies?.Where(dependency => dependency.Source == "nuget");
+
+            var aspNetCoreAllDependency = nugetDependencies.FirstOrDefault(dependency => dependency.Name == "Microsoft.AspNetCore.All");
+
+            if (aspNetCoreAllDependency != null)
+            {
+                var typeAndImplementations = new RepositoryTypeAndImplementations();
+                typeAndImplementations.TypeName = "API";
+
+                typeAndImplementations.Implementations = new List<RepositoryImplementation>
+                {
+                    new RepositoryImplementation
+                    {
+                        Name = "ASP.NET Core",
+                        Version = aspNetCoreAllDependency.Version,
+                        MajorVersion = aspNetCoreAllDependency.Version.GetMajorVersion()
+                    },
+                    new RepositoryImplementation
+                    {
+                        Name = "Web API"
+                    }
+                };
+
+                return typeAndImplementations;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/RepositoryAnalyticsApi.Extensions/TypeAndImplementation/DotNetCoreConsoleApplication.cs
@@ -0,0 +1,54 @@
+﻿using RepositoryAnalyticsApi.Extensibility;
+using RepositoryAnalyticsApi.Extensibliity;
+using RepositoryAnalyticsApi.ServiceModel;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RepositoryAnalyticsApi.Extensions.TypeAndImplementation
+{
+    [Export(typeof(IDeriveRepositoryTypeAndImplementations))]
+    public class DotNetCoreConsoleApplication : IDeriveRepositoryTypeAndImplementations
+    {
+        public async Task<RepositoryTypeAndImplementations> DeriveImplementationAsync(IEnumerable<RepositoryDependency> dependencies, Func<Task<List<RepositoryFile>>> readFilesAsync, IEnumerable<string> topics, string name, Func<string, Task<string>> readFileContentAsync)
+        {
+            var files = await readFilesAsync();
+            var projFiles = files.Where(file => file.FullPath.EndsWith(".csproj") || file.FullPath.EndsWith(".vbproj"));
+
+            foreach (var projFile in projFiles)
+            {
+                var projectFileContent = await readFileContentAsync(projFile.FullPath).ConfigureAwait(false);
+
+                var xDoc = XmlHelper.RemoveUtf8ByteOrderMarkAndParse(projectFileContent);
+
+                var targetFrameworkElement = xDoc.Descendants().FirstOrDefault(descendant => descendant.Name.LocalName == "TargetFramework");
+                var outputTypeElement = xDoc.Descendants().FirstOrDefault(descendant => descendant.Name.LocalName == "OutputType");
+
+                var isNetCoreApplication = targetFrameworkElement != null && targetFrameworkElement.Value.StartsWith("netcoreapp");
+                var isExeOutputType = outputTypeElement != null && outputTypeElement.Value == "Exe";
+
+                if (isNetCoreApplication && isExeOutputType)
+                {
+                    var typeAndImplementations = new RepositoryTypeAndImplementations();
+                    typeAndImplementations.TypeName = "Console Application";
+
+                    typeAndImplementations.Implementations = new List<RepositoryImplementation>
+                    {
+                        new RepositoryImplementation
+                        {
+                            Name = ".NET Core",
+                        }
+                    };
+
+                    return typeAndImplementations;
+                }
+            }
+
+            return null;
+        }
+    }
+}
