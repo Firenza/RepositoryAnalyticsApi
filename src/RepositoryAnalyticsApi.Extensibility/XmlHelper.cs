b9a14823f18542ac7aee63a2fd6b4d6bef03416d
Firenza <chadoftedahl@gmail.com>
2018-02-24 18:21:50 -0600

    Moved some shared code

---
+++ src/RepositoryAnalyticsApi.Extensibility/XmlHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace RepositoryAnalyticsApi.Extensibility
+{
+    public static class XmlHelper
+    {
+        /// <summary>
+        /// Removes any UTF-8 Byte Order Mark before parsing into XDocument
+        /// </summary>
+        /// <param name="xmlContent"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// .NET project files will ocassionally fail to be parsed into XDocuments due to a having a BOM.
+        /// </remarks>
+        public static XDocument RemoveUtf8ByteOrderMarkAndParse(string xmlContent)
+        {
+            string byteOrderMarkUtf8 = Encoding.UTF8.GetString(Encoding.UTF8.GetPreamble());
+            if (xmlContent.StartsWith(byteOrderMarkUtf8))
+            {
+                xmlContent = xmlContent.Remove(0, byteOrderMarkUtf8.Length);
+
+                // For some reason removing the UTF preamble sometimes removes the first XML character
+                if (!xmlContent.StartsWith("<"))
+                {
+                    xmlContent = $"<{xmlContent}";
+                }
+            }
+
+            return XDocument.Parse(xmlContent);
+        }
+    }
+}
