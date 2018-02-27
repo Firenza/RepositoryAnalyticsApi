e0e1d31332e9e40e7be7612b94ee1d46ee6882f8
Firenza <chadoftedahl@gmail.com>
2018-02-27 17:49:53 -0600

    Added DevOps integration info

---
+++ src/RepositoryAnalyticsApi.Extensibility/IDeriveRepositoryDevOpsIntegrations.cs
@@ -0,0 +1,15 @@
+﻿using RepositoryAnalyticsApi.ServiceModel;
+using System.Threading.Tasks;
+
+namespace RepositoryAnalyticsApi.Extensibility
+{
+    public interface IDeriveRepositoryDevOpsIntegrations
+    {
+        /// <summary>
+        /// Derives the level of DevOps integration the given repository has
+        /// </summary>
+        /// <param name="repositoryName"></param>
+        /// <returns></returns>
+        Task<RepositoryDevOpsIntegrations> DeriveIntegrationsAsync(string repositoryName); 
+    }
+}
---
+++ src/RepositoryAnalyticsApi.ServiceModel/RepositoryDevOpsIntegrations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryAnalyticsApi.ServiceModel
+{
+    public class RepositoryDevOpsIntegrations
+    {
+        public bool ContinuousIntegration { get; set; }
+        public bool ContinuousDelivery { get; set; }
+        public bool ContinuousDeployment { get; set; }
+    }
+}
