01ed1ffb9d78902b48ff664a49a0af628bc90352
Firenza <chadoftedahl@gmail.com>
2018-07-22 14:03:24 -0500

    Added time filter for typename search

--- src/RepositoryAnaltyicsApi.Interfaces/IRepositoriesTypeNamesManager.cs
+++ src/RepositoryAnaltyicsApi.Interfaces/IRepositoriesTypeNamesManager.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace RepositoryAnaltyicsApi.Interfaces
 {
     public interface IRepositoriesTypeNamesManager
     {
-        Task<List<string>> ReadAsync();
+        Task<List<string>> ReadAsync(DateTime? asOf);
     }
 }
