2d5fb11dccedba6c94884b8e99584fd90c665678
Firenza <chadoftedahl@gmail.com>
2018-07-28 09:05:52 -0500

    Added some custom query string validation

--- src/RepositoryAnaltyicsApi.Controllers/RepositoriesImplementationsController.cs
+++ src/RepositoryAnaltyicsApi.Controllers/RepositoriesImplementationsController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using RepositoryAnaltyicsApi.Interfaces;
 using RepositoryAnalyticsApi.ServiceModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace RepositoryAnaltyicsApi.Controllers
@@ -30,6 +32,14 @@ namespace RepositoryAnaltyicsApi.Controllers
         [HttpGet]
         public async Task<ActionResult<List<IntervalCountAggregations>>> GetAsync(string repositoryTypeName, DateTime? intervalStartTime, DateTime? intervalEndTime, int? intervals)
         {
+            if (intervals.HasValue && (!intervalStartTime.HasValue && !intervalEndTime.HasValue))
+            {
+                var modelStateDictionary = new ModelStateDictionary();
+                modelStateDictionary.TryAddModelError(nameof(intervals), "Can not specify an interval without a start or end time");
+
+                return BadRequest(modelStateDictionary);
+            }
+
             var intervalCountAggregations =  await repositoryImplementationsManager.SearchAsync(repositoryTypeName, intervalStartTime, intervalEndTime, intervals);
 
             return intervalCountAggregations;
