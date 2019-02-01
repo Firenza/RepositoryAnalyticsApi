c01033524fe1e64bd345697a5045ec4c5ef1130a
Firenza <chadoftedahl@gmail.com>
2019-02-01 15:54:31 -0800

    MySql snapshot timings

--- src/RepositoryAnalyticsApi.Repositories/MySqlRepositorySnapshotRepository.cs
+++ src/RepositoryAnalyticsApi.Repositories/MySqlRepositorySnapshotRepository.cs
@@ -11,6 +11,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SerilogTimings;
 
 namespace RepositoryAnalyticsApi.Repositories
 {
@@ -46,72 +47,89 @@ namespace RepositoryAnalyticsApi.Repositories
 
             using (var mySqlConnection = new MySqlConnection(mySqlConnectionString))
             {
-                var timer = Stopwatch.StartNew();
-
                 await mySqlConnection.OpenAsync();
 
-                var existingRecordId = await mySqlConnection.ExecuteScalarAsync<int>(
+                int existingRecordId = 0;
+
+                using (Operation.Time("Snapshot Find"))
+                {
+                    existingRecordId = await mySqlConnection.ExecuteScalarAsync<int>(
                          @"SELECT Id 
                       FROM RepositorySnapshots
                       WHERE WindowStartCommitId = @WindowStartCommitId",
                          new { WindowStartCommitId = snapshot.WindowStartCommitId });
+                }
 
                 if (existingRecordId == 0)
                 {
-                    existingRecordId = await mySqlConnection.InsertAsync(mappedRepositorySnapshot);
+                    using (Operation.Time("Snapshot Insert"))
+                    {
+                        existingRecordId = await mySqlConnection.InsertAsync(mappedRepositorySnapshot);
+                    }
                 }
                 else
                 {
                     mappedRepositorySnapshot.Id = existingRecordId;
 
-                    await mySqlConnection.UpdateAsync(mappedRepositorySnapshot);
+                    using (Operation.Time("Snapshot Update"))
+                    {
+                        await mySqlConnection.UpdateAsync(mappedRepositorySnapshot);
+                    }
 
-                    // For now just always delete all existing child tables
-                    await mySqlConnection.ExecuteAsync(
-                         @"DELETE 
+                    using (Operation.Time("Snapshot Children Deletion"))
+                    {
+                        // For now just always delete all existing child tables
+                        await mySqlConnection.ExecuteAsync(
+                             @"DELETE 
                         FROM RepositoryDependencies
                         WHERE RepositorySnapshotId = @RepositorySnapshotId",
-                         new { RepositorySnapshotId = existingRecordId });
+                             new { RepositorySnapshotId = existingRecordId });
 
-                    await mySqlConnection.ExecuteAsync(
-                        @"DELETE 
+                        await mySqlConnection.ExecuteAsync(
+                            @"DELETE 
                         FROM RepositoryFiles
                         WHERE RepositorySnapshotId = @RepositorySnapshotId",
-                        new { RepositorySnapshotId = existingRecordId });
+                            new { RepositorySnapshotId = existingRecordId });
 
-                    // Foreign key constraint will cascade delete any child implementations
-                    await mySqlConnection.ExecuteAsync(
-                       @"DELETE 
+                        // Foreign key constraint will cascade delete any child implementations
+                        await mySqlConnection.ExecuteAsync(
+                           @"DELETE 
                         FROM RepositoryTypes
                         WHERE RepositorySnapshotId = @RepositorySnapshotId",
-                       new { RepositorySnapshotId = existingRecordId });
+                           new { RepositorySnapshotId = existingRecordId });
+                    }
                 }
 
                 var mappedFiles = Model.MySql.RepositoryFile.MapFrom(snapshot, existingRecordId);
 
-                await mySqlConnection.InsertAsync(mappedFiles);
+                using (Operation.Time($"Snapshot {mappedFiles.Count} Files Insert"))
+                {
+                    await mySqlConnection.InsertAsync(mappedFiles);
+                }
 
                 var mappedDependencies = Model.MySql.RepositoryDependency.MapFrom(snapshot, existingRecordId);
 
-                await mySqlConnection.InsertAsync(mappedDependencies);
+                using (Operation.Time($"Snapshot {mappedDependencies.Count} Dependencies Insert"))
+                {
+                    await mySqlConnection.InsertAsync(mappedDependencies);
+                }
 
                 var mappedTypes = Model.MySql.RepositoryType.MapFrom(snapshot, existingRecordId);
 
-                foreach (var mappedType in mappedTypes)
+                using (Operation.Time("Snapshot Types and Implementations Insert"))
                 {
-                    var typeId = await mySqlConnection.InsertAsync(mappedType);
+                    foreach (var mappedType in mappedTypes)
+                    {
+                        var typeId = await mySqlConnection.InsertAsync(mappedType);
 
-                    // Get matching implementation list
-                    var matchingTypeAndImplementation = snapshot.TypesAndImplementations.FirstOrDefault(typeAndImplementation => typeAndImplementation.TypeName == mappedType.Name);
+                        // Get matching implementation list
+                        var matchingTypeAndImplementation = snapshot.TypesAndImplementations.FirstOrDefault(typeAndImplementation => typeAndImplementation.TypeName == mappedType.Name);
 
-                    var mappedImplementations = Model.MySql.RepositoryImplementation.MapFrom(matchingTypeAndImplementation.Implementations, typeId);
+                        var mappedImplementations = Model.MySql.RepositoryImplementation.MapFrom(matchingTypeAndImplementation.Implementations, typeId);
 
-                    await mySqlConnection.InsertAsync(mappedImplementations);
+                        await mySqlConnection.InsertAsync(mappedImplementations);
+                    }
                 }
-
-                timer.Stop();
-
-                logger.LogDebug($"Upsert of {nameof(RepositorySnapshot)} took {timer.ElapsedMilliseconds} milliseconds");
             }
         }
     }
