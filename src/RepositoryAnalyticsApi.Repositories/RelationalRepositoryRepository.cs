ab97aaac83b77d660eca42d29f544e16a5498375
Chad Oftedhal <chadoftedahl@gmail.com>
2019-05-18 14:57:33 -0700

    Working multiple and single repository read

--- src/RepositoryAnalyticsApi.Repositories/RelationalRepositoryRepository.cs
+++ src/RepositoryAnalyticsApi.Repositories/RelationalRepositoryRepository.cs
@@ -25,68 +25,206 @@ namespace RepositoryAnalyticsApi.Repositories
 
         public async Task<Repository> ReadAsync(string repositoryId, DateTime? asOf)
         {
-            var dbConnection = repositoryAnalysisContext.Database.GetDbConnection();
-
-            var respositoryDependencySearchResults = await dbConnection.QueryAsync<Model.EntityFramework.RepositoryCurrentState>(
-                @"select RS.*
-                    from repository_current_state RS
-                    limit 3 
-                    offset 2");
-
-
-
             var dbRepositoryCurrentState = await repositoryAnalysisContext
-                                       .RepositoryCurrentState
-                                       .AsNoTracking()
-                                       .Include(rcs => rcs.RepositorySnapshots)
-                                         .ThenInclude(rs => rs.Dependencies)
-                                       .Include(rcs => rcs.RepositorySnapshots)
-                                         .ThenInclude(rs => rs.Files)
-                                       .Include(rcs => rcs.RepositorySnapshots)
-                                         .ThenInclude(rs => rs.TypesAndImplementations)
-                                             .ThenInclude(rti => rti.Implementations)
-                                       .Where(rcs =>
-                                            rcs.RepositoryId == repositoryId &&
-                                            rcs.RepositorySnapshots.Any(rs =>
-                                                !asOf.HasValue && rs.WindowEndsOn == null ||
-                                                asOf.HasValue && rs.WindowStartsOn < asOf.Value && rs.WindowEndsOn > asOf.Value))
-                                       .SingleOrDefaultAsync();
+                                .RepositoryCurrentState
+                                .Include(rcs => rcs.RepositorySnapshots)
+                                  .ThenInclude(rs => rs.Dependencies)
+                                .Include(rcs => rcs.RepositorySnapshots)
+                                  .ThenInclude(rs => rs.Files)
+                                .Include(rcs => rcs.RepositorySnapshots)
+                                  .ThenInclude(rs => rs.TypesAndImplementations)
+                                      .ThenInclude(rti => rti.Implementations)
+                                .Where(rcs =>
+                                     rcs.RepositoryId == repositoryId &&
+                                     rcs.RepositorySnapshots.Any(rs =>
+                                         !asOf.HasValue && rs.WindowEndsOn == null ||
+                                         asOf.HasValue && rs.WindowStartsOn < asOf.Value && rs.WindowEndsOn > asOf.Value))
+                                .SingleOrDefaultAsync();
 
-            var repositorySnapshot = mapper.Map<ServiceModel.RepositorySnapshot>(dbRepositoryCurrentState.RepositorySnapshots.FirstOrDefault());
-            var repositoryCurrentState = mapper.Map<ServiceModel.RepositoryCurrentState>(dbRepositoryCurrentState);
-
-            return new Repository
+            var repository = new Repository
             {
-                CurrentState = repositoryCurrentState,
-                Snapshot = repositorySnapshot
+                CurrentState = mapper.Map<ServiceModel.RepositoryCurrentState>(dbRepositoryCurrentState),
+                Snapshot = mapper.Map<ServiceModel.RepositorySnapshot>(dbRepositoryCurrentState.RepositorySnapshots.FirstOrDefault())
             };
+
+            return repository;
+
+            //var dbConnection = repositoryAnalysisContext.Database.GetDbConnection();
+
+            //var dbrcss = new List<Model.EntityFramework.RepositoryCurrentState>();
+
+            //var respositoryDependencySearchResults = await dbConnection.QueryAsync<Model.EntityFramework.RepositoryCurrentState, Model.EntityFramework.RepositorySnapshot, Model.EntityFramework.RepositoryDependency, ServiceModel.Repository>(
+            //    @"select * 
+            //        from repository_current_state RCS
+            //        join repository_snapshot RS
+	           //         on RS.repository_current_state_id = RCS.repository_current_state_id
+            //        join repository_dependency RD
+	           //         on RD.repository_snapshot_id = RS.repository_snapshot_id
+            //        where RCS.repository_id = @RepositoryId",
+
+            //(rcs, rs, rd) =>
+            //{
+            //    var dbrcs = dbrcss.FirstOrDefault(dbrcsx => dbrcsx.RepositoryCurrentStateId == rcs.RepositoryCurrentStateId);
+
+            //    // First add the rcs
+            //    if (dbrcs == null)
+            //    {
+            //        dbrcss.Add(rcs);
+            //        dbrcs = rcs;
+            //    }
+
+            //    if(dbrcs.RepositorySnapshots == null)
+            //    {
+            //        dbrcs.RepositorySnapshots = new List<Model.EntityFramework.RepositorySnapshot>();
+            //    }
+
+            //    var rsnappp = dbrcs.RepositorySnapshots?.FirstOrDefault(rsnap => rsnap.RepositorySnapshotId == rsnap.RepositorySnapshotId);
+
+            //    if (rsnappp == null)
+            //    {
+            //        dbrcs.RepositorySnapshots.Add(rs);
+            //        rsnappp = rs;
+            //    }
+
+            //    if (rsnappp.Dependencies == null)
+            //    {
+            //        rsnappp.Dependencies = new List<Model.EntityFramework.RepositoryDependency>();
+            //    }
+
+            //    var dep = rsnappp.Dependencies.FirstOrDefault(xyz => xyz.RepositoryDependencyId == rd.RepositoryDependencyId);
+
+            //    if (dep == null)
+            //    {
+            //        rsnappp.Dependencies.Add(rd);
+            //        dep = rd;
+            //    }
+
+            //    return null;
+            //},
+            //new { RepositoryId = repositoryId },
+            //splitOn: "repository_snapshot_id, repository_dependency_id");
+
+            //var repositories = new List<ServiceModel.Repository>();
+
+            //foreach (var x in dbrcss)
+            //{
+            //    var repositorySnapshot = mapper.Map<ServiceModel.RepositorySnapshot>(x.RepositorySnapshots.FirstOrDefault());
+            //    var repositoryCurrentState = mapper.Map<ServiceModel.RepositoryCurrentState>(x);
+
+            //    var newR =  new Repository
+            //    {
+            //        CurrentState = repositoryCurrentState,
+            //        Snapshot = repositorySnapshot
+            //    };
+
+            //    repositories.Add(newR);
+            //}
+
+
+            //return repositories.FirstOrDefault();
         }
 
         public async Task<List<ServiceModel.Repository>> ReadMultipleAsync(DateTime? asOf, int? page, int? pageSize)
         {
-            var dbRepositoryCurrentState = repositoryAnalysisContext
-                            .RepositoryCurrentState
-                            .AsNoTracking()
-                            .Include(rcs => rcs.RepositorySnapshots)
-                              .ThenInclude(rs => rs.Dependencies)
-                            .Include(rcs => rcs.RepositorySnapshots)
-                              .ThenInclude(rs => rs.Files)
-                            .Include(rcs => rcs.RepositorySnapshots)
-                              .ThenInclude(rs => rs.TypesAndImplementations)
-                                  .ThenInclude(rti => rti.Implementations)
-                            .Where(rcs =>
-                                 rcs.RepositorySnapshots.Any(rs =>
-                                     !asOf.HasValue && rs.WindowEndsOn == null ||
-                                     asOf.HasValue && rs.WindowStartsOn < asOf.Value && rs.WindowEndsOn > asOf.Value));
+            var dbConnection = repositoryAnalysisContext.Database.GetDbConnection();
 
+            // First get all the Id's of the repoistory records in this "page"
+            var repositoryIdsInPage = await dbConnection.QueryAsync<string>(
+              @" select repository_id
+                    from repository_current_state
+                    limit @limit
+                    offset @offset",
+                new { limit = pageSize.Value, offset = page.Value * pageSize.Value });
 
-            dbRepositoryCurrentState.Skip(pageSize.Value * page.Value).Take(pageSize ?? 10);
+            var repositories = new List<Repository>();
 
-            var repositoryCurrentStates = await dbRepositoryCurrentState.ToListAsync();
+            foreach (var repositoryId in repositoryIdsInPage)
+            {
+                var repository = await ReadAsync(repositoryId, asOf);
 
-            var repositories = mapper.Map<List<ServiceModel.Repository>>(repositoryCurrentStates);
+                repositories.Add(repository);
+            }
 
             return repositories;
+
+          //var dbConnection = repositoryAnalysisContext.Database.GetDbConnection();
+
+          //  var dbrcss = new List<Model.EntityFramework.RepositoryCurrentState>();
+
+          //  var respositoryDependencySearchResults = await dbConnection.QueryAsync<Model.EntityFramework.RepositoryCurrentState, Model.EntityFramework.RepositorySnapshot, Model.EntityFramework.RepositoryDependency, ServiceModel.Repository>(
+          //      @"select * 
+          //          from repository_current_state RCS
+          //          join repository_snapshot RS
+	         //           on RS.repository_current_state_id = RCS.repository_current_state_id
+          //          join repository_dependency RD
+	         //           on RD.repository_snapshot_id = RS.repository_snapshot_id
+          //          where RCS.repository_current_state_id in (
+          //              select repository_current_state_id
+          //              from repository_current_state
+          //              limit @limit
+          //              offset @offset
+          //          )",
+
+          //  (rcs, rs, rd) =>
+          //  {
+          //      var dbrcs = dbrcss.FirstOrDefault(dbrcsx => dbrcsx.RepositoryCurrentStateId == rcs.RepositoryCurrentStateId);
+
+          //      // First add the rcs
+          //      if (dbrcs == null)
+          //      {
+          //          dbrcss.Add(rcs);
+          //          dbrcs = rcs;
+          //      }
+
+          //      if (dbrcs.RepositorySnapshots == null)
+          //      {
+          //          dbrcs.RepositorySnapshots = new List<Model.EntityFramework.RepositorySnapshot>();
+          //      }
+
+          //      var rsnappp = dbrcs.RepositorySnapshots?.FirstOrDefault(rsnap => rsnap.RepositorySnapshotId == rsnap.RepositorySnapshotId);
+
+          //      if (rsnappp == null)
+          //      {
+          //          dbrcs.RepositorySnapshots.Add(rs);
+          //          rsnappp = rs;
+          //      }
+
+          //      if (rsnappp.Dependencies == null)
+          //      {
+          //          rsnappp.Dependencies = new List<Model.EntityFramework.RepositoryDependency>();
+          //      }
+
+          //      var dep = rsnappp.Dependencies.FirstOrDefault(xyz => xyz.RepositoryDependencyId == rd.RepositoryDependencyId);
+
+          //      if (dep == null)
+          //      {
+          //          rsnappp.Dependencies.Add(rd);
+          //          dep = rd;
+          //      }
+
+          //      return null;
+          //  },
+          //  new { limit = pageSize.Value, offset = page.Value * pageSize.Value},
+          //  splitOn: "repository_snapshot_id, repository_dependency_id");
+
+          //  var repositories = new List<ServiceModel.Repository>();
+
+          //  foreach (var x in dbrcss)
+          //  {
+          //      var repositorySnapshot = mapper.Map<ServiceModel.RepositorySnapshot>(x.RepositorySnapshots.FirstOrDefault());
+          //      var repositoryCurrentState = mapper.Map<ServiceModel.RepositoryCurrentState>(x);
+
+          //      var newR = new Repository
+          //      {
+          //          CurrentState = repositoryCurrentState,
+          //          Snapshot = repositorySnapshot
+          //      };
+
+          //      repositories.Add(newR);
+          //  }
+
+
+          //  return repositories;
         }
 
     }
